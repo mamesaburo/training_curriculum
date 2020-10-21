268c36ed96110084aacd92fc0e0943443238e273
mamesaburo <cinnabar.b.m.b.9597@gmail.com>
2020-10-21 21:46:23 +0900

    Issue6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -19,7 +19,7 @@ class CalendarsController < ApplicationController
   end
 
   def getWeek
-    wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
+    @wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
 
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
@@ -35,21 +35,14 @@ class CalendarsController < ApplicationController
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
 
-      #wday_num = #Date.today.wdayを利用して添字となる数値を得る
+      @wday_num = Date.today.wday + x
       #もしもwday_numが7以上であれば、7を引く
-      #if #条件式を記述
-        #wday_num = wday_num - 7
-      #end
+      if @wday_num >= 7
+        @wday_num = @wday_num - 7
+      end
 
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans, wday: wdays}
+      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans, wday: @wdays[@wday_num]}
       @week_days.push(days)
     end
-
   end
 end
-
-# 配列の取り出し方　配列[ 0 ]　wdays[Date.today.wday]
-#  Date.today.wday = 今日の日付の曜日を数字で出してくれます。日曜日　＝　０
-# xをどうしますか？
-# ビューファイルの編集も忘れずに
-
